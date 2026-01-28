864cedae300a2bb05982f767bd3d05bc99f53643
khalfa <khalfaradhouene@gmail.com>
2026-01-28 23:15:06 +0100

    feat:xmen

--- app/db.server.ts
+++ app/db.server.ts
@@ -1,9 +1,18 @@
 import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
 
-const prisma = (DATABASE_URL?: string) =>
-  new PrismaClient({
-    datasourceUrl: DATABASE_URL,
+// const prisma = (DATABASE_URL?: string) =>
+//   new PrismaClient({
+//     datasourceUrl: DATABASE_URL,
+//   }).$extends(withAccelerate());
+
+
+
+
+  const prisma = new PrismaClient({
+    datasources: {
+      db:process.env.DATABASE_URL, // <- directUrl for Prisma 7
+    },
   }).$extends(withAccelerate());
 
 export default prisma;
--- server.ts
+++ server.ts
@@ -23,6 +23,7 @@ export default {
             ctx: {
               waitUntil: ctx.waitUntil.bind(ctx),
               passThroughOnException: ctx.passThroughOnException.bind(ctx),
+              props:{}
             },
             caches,
             env,
